741719c37392e915220663d1361d4a99367953b0
Naif Adnan <146568103+adnan99999977@users.noreply.github.com>
2025-08-27 10:22:00 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['"Hind Madurai"', 'sans-serif'], // এখন তুমি class="font-hind" ব্যবহার করতে পারবে
+      },
+    },
+  },
+}
