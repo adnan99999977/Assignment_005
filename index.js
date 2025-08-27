ef41a557858c77c5e3094c0eb064c97749838eea
Naif Adnan <146568103+adnan99999977@users.noreply.github.com>
2025-08-27 22:47:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // তোমার project structure অনুযায়ী
+    "./src/**/*.{html,js}", 
   ],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'], // এখন 'font-inter' নামে ব্যবহার হবে
+        inter: ['Inter', 'sans-serif'], 
       },
     },
   },
