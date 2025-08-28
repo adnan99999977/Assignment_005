8db8c77616b340abe781b794a783d5bacd6e6ef4
Naif Adnan <146568103+adnan99999977@users.noreply.github.com>
2025-08-28 21:47:15 +0600

    clear btn and copy btn solved

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ copyBtns.forEach((btn, index) => {
         let copyBox = getInnerTextIntoNum("copy_box");
         let newText = copyBox + 1 + " " + "copy";
         document.getElementById("copy_box").children[0].innerText = newText;
-        let copyNum = number[index];
+        let copyNum = numbers[index];
         navigator.clipboard.writeText(copyNum);
         alert("Hotline number copied successfully 😊");
     });
@@ -77,13 +77,12 @@ callBtn.forEach((btn, index) => {
             </div></div>`
     historyList.appendChild(newChild)
        
-    let clearBtn = document.getElementById('clear_btn')
-    clearBtn.addEventListener('click',function(){
-         document.getElementById("history_list").innerHTML = "";
-        newChild.style.display = 'block'
-    })
-    
+  })
+  
 });
+let clearBtn = document.getElementById('clear_btn')
+clearBtn.addEventListener('click',function(){
+     document.getElementById("history_list").innerHTML = "";
 });
 
 
